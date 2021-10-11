e912091ad4f39632999024adbe5deecefc3ca4b2
shReya <shetshreya01@gmail.com>
2021-10-11 22:21:59 +0530

    first commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Hello from "./Hello";
+import Hello2 from "./Hello2";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello2 />
+    </div>
+  );
+}
+export default App;
---
+++ reactapp/src/Hello2.js
@@ -0,0 +1,10 @@
+function Hello2() {
+  return (
+    <div>
+      <p> hey, how are you?</p>
+      <p> hey, how are you?</p>
+      <p> hey, how are you?</p>
+    </div>
+  );
+}
+export default Hello2;
